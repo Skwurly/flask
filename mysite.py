9926ec962ba4a46da64617a5d358b0d3a2d340e5
urlybyrd <urlscube@gmail.com>
2017-09-19 03:34:24 -0600

    second commit: added timestamp

--- mysite.py
+++ mysite.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
-
+import datetime
 app = Flask('__name__')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    now = datetime.datetime.now()
+    return render_template('index.html', now=now)
 
 if __name__ == ('__main__'):
     app.run(debug=True)
